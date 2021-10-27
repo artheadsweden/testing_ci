b401949a1ea5b1979f61850c59f79353bb0abe9d
Joakim Wassberg <joakim@arthead.se>
2021-10-27 11:20:06 +0200

    Added robber's language translation function

--- test_string_ops.py
+++ test_string_ops.py
@@ -7,3 +7,6 @@ class TestStringOps:
 
     def test_reverse_string(self):
         assert string_ops.reverse_string("Joakim") == "mikaoJ"
+
+    def test_translate_to_robber(self):
+        assert string_ops.translate_to_robber("thIs is fun") == "tothohisos isos fofunon"
